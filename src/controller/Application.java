422348a891ecc006c054e4a159126383f0f4c506
demitech1024 <demitech1024@gmail.com>
2020-03-07 19:29:51 -0500

    Can finally read the words.json file now i need to make it an object

--- src/controller/Application.java
+++ src/controller/Application.java
@@ -12,18 +12,14 @@ import javax.swing.*;
 import graphics.Display;
 
 public class Application {
-    public static void main(String[] args) {
-        BufferedReader jsonFile = null;
-        try {
-            jsonFile = new BufferedReader(new FileReader("../data/words.json"));
-        } catch (Exception IOException) {
-            System.out.println("Could not find words.json file."); 
-        }
+    public static void main(String[] args) throws IOException {
+                
+        BufferedReader jsonFile = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/src/data/words.json"));
 
         
 
-        JFrame frame = Display.createFrame();
-        frame.pack();
-        frame.setVisible(true);
+        // JFrame frame = Display.createFrame();
+        // frame.pack();
+        // frame.setVisible(true);
     }
 }
