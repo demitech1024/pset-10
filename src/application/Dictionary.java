7137f2aa874c908678b36e1707af1c4e58fb0f8f
demitech1024 <demitech1024@gmail.com>
2020-03-11 23:25:13 -0400

    Dictionary

--- src/application/Dictionary.java
+++ src/application/Dictionary.java
@@ -59,10 +59,6 @@ public class Dictionary {
   private JTextField textFieldA;
   private JTextField textFieldC;
 
-  /**
-   * Launch the application.
-   * @throws FileNotFoundException
-   */
   public static void main(String[] args) throws FileNotFoundException {
     getWords();
     EventQueue.invokeLater(new Runnable() {
